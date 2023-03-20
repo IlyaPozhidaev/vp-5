583b4ed7dcbaa068f2b0efe4a48f200fea8c9b8f
IlyaPozhidaev <99234640+IlyaPozhidaev@users.noreply.github.com>
2023-03-20 22:58:31 +0700

    apps

---
+++ Editor/Models/Convertes.cs
@@ -0,0 +1,38 @@
+﻿using Avalonia;
+using Avalonia.Media;
+using System.Collections.Generic;
+
+namespace Editor.Models
+{
+    public static class Converters
+    {
+        public static string BrushToString(SolidColorBrush brush) => brush.ToString();
+
+        public static SolidColorBrush StringToBrush(string str) => new SolidColorBrush(Color.Parse(str));
+
+        public static string PointToString(Point p) => p.ToString();
+
+        public static Point StringToPoint(string str)
+        {
+            var s = str.Split(',');
+            return new Point(double.Parse(s[0]), double.Parse(s[1]));
+        }
+
+        public static List <Point> StringToPoints(string str)
+        {
+            var list = new List <Point>();
+            var s = str.Split(',');
+            for (int i = 0; i < s.Length; i += 2)
+            {
+                list.Add(new Point(double.Parse(s[i]), double.Parse(s[i + 1])));
+            }
+            return list;
+        }
+
+        public static Thickness StringToMargin(string str)
+        {
+            var s = str.Split(',');
+            return new Thickness(double.Parse(s[0]), double.Parse(s[1]));
+        }
+    }
+}
---
+++ Editor/Models/Figures.cs
@@ -0,0 +1,16 @@
+﻿using Editor.Models;
+using System.Xml.Serialization;
+
+namespace Editor.Models
+{
+    [XmlInclude(typeof(LineElement))]
+    [XmlInclude(typeof(PolylineElement))]
+    [XmlInclude(typeof(PolygonElement))]        
+    [XmlInclude(typeof(RectangleElement))]
+    [XmlInclude(typeof(EllipseElement))]
+    [XmlInclude(typeof(PathElement))]
+    public class Figures
+    {
+        public string Name { get; set; }
+    }
+}
